c31eaf58282842c78bf953a48765dbaa4acc4edb
Kevin Savage <kevin.savage@diamond.ac.uk>
2017-05-30 11:42:59 +0100

    fix generation of test data

--- fiducials/data_creator.py
+++ fiducials/data_creator.py
@@ -1,35 +1,54 @@
 import numpy as np
 from scipy.ndimage.interpolation import rotate
-from functools import partial
 
-def create_example_images(number_of_images, start_angle, end_angle, number_of_fiducials, radius_of_fiducials, volume_dims, image_dims):
+
+def create_example_images(number_of_images, start_angle, end_angle, number_of_fiducials, radius_of_fiducials,
+                          volume_dims, image_dims):
     fiducials = np.random.rand(number_of_fiducials, 3) * volume_dims
     angles = generate_angles(number_of_images, start_angle, end_angle)
     volume = generate_volume_from_fiducials(fiducials, volume_dims, radius_of_fiducials)
     return angles, np.array(list(project_fiducials(volume, angles, image_dims)))
 
+
 def generate_angles(number_of_images, start_angle, end_angle):
     return np.arange(start_angle, end_angle, (end_angle - start_angle) / number_of_images)
 
+
 def generate_volume_from_fiducials(fiducials, volume_dims, radius_of_fiducials):
-    volume = np.ones(shape = (volume_dims,volume_dims,volume_dims)) > 0
+    volume = np.zeros(shape=(volume_dims, volume_dims, volume_dims))
     for fiducial in fiducials:
-        volume = volume * generate_volume_from_fiducial(fiducial, volume_dims, radius_of_fiducials)
+        volume += generate_volume_from_fiducial(fiducial, volume_dims, radius_of_fiducials)
     return volume
-    
+
+
 def generate_volume_from_fiducial(fiducial, volume_dims, radius):
     axis = range(0, volume_dims)
-    x,y,z = np.meshgrid(axis, axis, axis)
-    return ((x - fiducial[0])**2 + (y - fiducial[1])**2 + (z - fiducial[2])**2) ** 0.5 > radius
+    x, y, z = np.meshgrid(axis, axis, axis)
+    return (((x - fiducial[0]) ** 2 + (y - fiducial[1]) ** 2 + (z - fiducial[2]) ** 2) ** 0.5 < radius).astype(int)
+
 
 def project_fiducials(volume, angles, image_dims):
     for angle in angles:
-        rotated = rotate(volume, angle, axes=(0,1))
-        product = np.prod(rotated, axis=0)
+        rotated = rotate(volume, angle, axes=(0, 1))
+        product = np.sum(rotated, axis=0)
         dim = product.shape[0]
-        minx = (dim-image_dims)/2
-        maxx = (dim+image_dims)/2
-        yield product[minx:maxx,minx:maxx]
+        minx = (dim - image_dims) / 2
+        maxx = (dim + image_dims) / 2
+        cropped = product[minx:maxx, minx:maxx]
+        normalised = (cropped > .3).astype(int)
+        yield normalised
 
-if __name__ == "__main__":
-   
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    angles, images = create_example_images(
+        number_of_images=10,
+        start_angle=10,
+        end_angle=20,
+        number_of_fiducials=80,
+        radius_of_fiducials=5,
+        volume_dims=200,
+        image_dims=120
+    )
+    plt.imshow(images[0])
+    plt.show()
--- tests/test_data_creator.py
+++ tests/test_data_creator.py
@@ -1,57 +1,60 @@
-import numpy as np
 from fiducials.data_creator import *
+
 
 def test_create_example_images():
     angles, images = create_example_images(
-        number_of_images = 10,
-        start_angle = 10,
-        end_angle = 20,
-        number_of_fiducials = 80,
-        radius_of_fiducials = 5,
-        volume_dims = 200,
-        image_dims = 120
+        number_of_images=10,
+        start_angle=10,
+        end_angle=20,
+        number_of_fiducials=80,
+        radius_of_fiducials=5,
+        volume_dims=200,
+        image_dims=120
     )
 
     # check some high level properties
-    assert images.shape == (10, 100, 100)
-    assert (images == 1 or images == 0).all()
-    
-    max_zeros_per_image = np.Pi * 25 * 80
-    pixels_per_image = 80*80
+    assert images.shape == (10, 120, 120)
+    assert ((images == 1) | (images == 0)).all()
+
+    max_zeros_per_image = np.pi * 25 * 80
+    pixels_per_image = 80 * 80
     min_ones_per_image = pixels_per_image - max_zeros_per_image
     min_average = min_ones_per_image / pixels_per_image
 
-    assert images.average > min_average
+    assert np.average(images) > min_average
+
 
 def test_generate_angles():
-    angles = generate_angles(number_of_images = 10, start_angle = 20, end_angle = 30)
-    assert (angles == np.array([20,21,22,23,24,25,26,27,28,29])).all()
+    angles = generate_angles(number_of_images=10, start_angle=20, end_angle=30)
+    assert (angles == np.array([20, 21, 22, 23, 24, 25, 26, 27, 28, 29])).all()
+
 
 def test_generate_volume_from_fiducials():
-    volume = generate_volume_from_fiducials(np.array([[100,100,100],[50,50,50]]), 200, 5)
+    volume = generate_volume_from_fiducials(np.array([[100, 100, 100], [50, 50, 50]]), 200, 5)
 
-    assert volume.shape == (200,200,200)
-    assert volume[100,100,100] == False
-    assert volume[50,50,50] == False
-    assert volume[150,150,150] == True
+    assert volume.shape == (200, 200, 200)
+    assert volume[100, 100, 100] == 1
+    assert volume[50, 50, 50] == 1
+    assert volume[150, 150, 150] == 0
+
 
 def test_generate_volume_from_fiducial():
-    volume = generate_volume_from_fiducial(np.array([100,100,100]), 200, 5)
+    volume = generate_volume_from_fiducial(np.array([100, 100, 100]), 200, 5)
 
-    assert volume.shape == (200,200,200)
-    assert volume[100,100,100] == False
-    assert volume[99,99,99] == False
-    assert volume[50,50,50] == True
+    assert volume.shape == (200, 200, 200)
+    assert volume[100, 100, 100] == 1
+    assert volume[99, 99, 99] == 1
+    assert volume[50, 50, 50] == 0
+
 
 def test_project_fiducials():
     projections = project_fiducials(
-        volume = np.ones((100,100,100)) > 0,
-        angles = [0, 45, 90],
-        image_dims = 20
+        volume=np.zeros((100, 100, 100)) > 0,
+        angles=[0, 45, 90],
+        image_dims=20
     )
-    
+
     result = np.array(list(projections))
 
-    assert result.shape == (3,20,20)
-    assert result.all()
-
+    assert result.shape == (3, 20, 20)
+    assert not result.any()
